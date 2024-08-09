4577a8224e9082c75cc745e7fe7241fc2f3dba7c
Huseyin Cenik <127469334+huseyincenik@users.noreply.github.com>
2024-08-09 14:27:00 +0300

    Add files via upload

--- Projects/ 8_Week_SQL_Challenge/Case_Study_2/Solution.sql
+++ Projects/ 8_Week_SQL_Challenge/Case_Study_2/Solution.sql
@@ -158,7 +158,10 @@ GROUP BY
 	) AS subquery;
 
 -- Question 7) For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
-
+SELECT
+	*
+FROM
+	pizza_runner.customer_orders;
 
 WITH order_changes AS (
 
@@ -199,7 +202,7 @@ GROUP BY
 	) AS subquery
 
 WHERE
-	exclusions_with_extras != 0
+	exclusions_with_extras != 0;
 
 
 -- Solution II
@@ -209,6 +212,63 @@ SELECT
 FROM
 	pizza_runner.customer_orders
 WHERE
-	exclusions IS NOT NULL AND extras IS NOT NULL
+	exclusions IS NOT NULL AND extras IS NOT NULL;
 
 -- Question 9) What was the total volume of pizzas ordered for each hour of the day?
+
+SELECT
+	*
+FROM
+	pizza_runner.customer_orders;
+
+
+SELECT
+	DATEPART(HOUR, order_time) AS order_hour,
+	COUNT(pizza_id) AS count_of_pizza
+	
+FROM
+	pizza_runner.customer_orders
+GROUP BY
+	DATEPART(HOUR, order_time)
+ORDER BY
+	order_hour ASC;
+
+-- Question 10)What was the volume of orders for each day of the week?
+
+SELECT
+	DATENAME(WEEKDAY, order_time) AS order_day,
+	DATEPART(DAY, order_time) AS order_day_2,
+	DATEPART(HOUR, order_time) AS order_hour,
+	COUNT(pizza_id) AS count_of_pizza
+	
+FROM
+	pizza_runner.customer_orders
+GROUP BY
+	DATEPART(HOUR, order_time),
+	DATENAME(WEEKDAY, order_time),
+	DATEPART(DAY, order_time)
+ORDER BY
+	order_day_2 ASC;
+
+-- B. Runner and Customer Experience
+-- Question 1) How many runners signed up for each 1 week period? (i.e. week starts 2021-01-01)
+SELECT
+	*
+FROM
+	pizza_runner.runners;
+
+WITH CTE AS (
+SELECT
+	registration_date,
+	DATEDIFF(WEEK, '2021-01-01', registration_date) + 1 AS week_number
+FROM
+	pizza_runner.runners) 
+SELECT
+	week_number,
+	COUNT(*) AS count_of_runners
+FROM
+	CTE
+GROUP BY
+	week_number
+ORDER BY
+	week_number;
