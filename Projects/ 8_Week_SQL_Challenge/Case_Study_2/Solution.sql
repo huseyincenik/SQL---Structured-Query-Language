be43bfef3d485ae1ba9341d85bcd369750a07866
Huseyin Cenik <127469334+huseyincenik@users.noreply.github.com>
2024-09-06 15:13:24 +0300

    Add files via upload

--- Projects/ 8_Week_SQL_Challenge/Case_Study_2/Solution.sql
+++ Projects/ 8_Week_SQL_Challenge/Case_Study_2/Solution.sql
@@ -233,22 +233,24 @@ GROUP BY
 ORDER BY
 	order_hour ASC;
 
--- Question 10)What was the volume of orders for each day of the week?
+select DATEPART(HOUR, order_time) AS order_hour,
+        count(order_id)
+from pizza_runner.customer_orders co
+group by DATEPART(HOUR, order_time)
+order by 1;
+
+-- Question 10) What was the volume of orders for each day of the week?
 
 SELECT
 	DATENAME(WEEKDAY, order_time) AS order_day,
 	DATEPART(DAY, order_time) AS order_day_2,
-	DATEPART(HOUR, order_time) AS order_hour,
 	COUNT(pizza_id) AS count_of_pizza
 	
 FROM
 	pizza_runner.customer_orders
 GROUP BY
-	DATEPART(HOUR, order_time),
 	DATENAME(WEEKDAY, order_time),
-	DATEPART(DAY, order_time)
-ORDER BY
-	order_day_2 ASC;
+	DATEPART(DAY, order_time);
 
 -- B. Runner and Customer Experience
 -- Question 1) How many runners signed up for each 1 week period? (i.e. week starts 2021-01-01)
@@ -260,7 +262,7 @@ FROM
 WITH CTE AS (
 SELECT
 	registration_date,
-	DATEDIFF(WEEK, '2021-01-01', registration_date) + 1 AS week_number
+	FLOOR(DATEDIFF(DAY, '2021-01-01', registration_date) / 7)  AS week_number
 FROM
 	pizza_runner.runners) 
 SELECT
@@ -272,3 +274,24 @@ GROUP BY
 	week_number
 ORDER BY
 	week_number;
+
+-- Question 2) What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
+
+SELECT
+	*
+FROM
+	pizza_runner.runner_orders;
+
+SELECT
+	runner_id,
+	AVG(duration) AS average_pickup_time
+FROM
+	pizza_runner.runner_orders
+WHERE
+	cancellation IS NULL -- for not cancelling order
+GROUP BY
+	runner_id
+ORDER BY
+	runner_id;
+
+-- Question 3) Is there any relationship between the number of pizzas and how long the order takes to prepare?
